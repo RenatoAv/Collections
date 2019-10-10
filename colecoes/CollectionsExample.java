190d5d02bf60c8132d7647399418ae0678ceb655
Thiago <tsousa900@gmail.com>
2019-10-10 15:31:12 -0300

    Utilizando operações com HashSet para mostrar sua diferença para o arrayList

--- colecoes/CollectionsExample.java
+++ colecoes/CollectionsExample.java
@@ -10,16 +10,21 @@ public class CollectionsExample {
 		int idades;
 		
 		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		Set<Pessoa> pessoasSemRepeticao = new HashSet<Pessoa>();
 		
+		
+		/*Operações Com ArrayList*/
 		pessoas.add(pessoa1);
 		pessoas.add(pessoa2);
 		pessoas.add(pessoa3);
 		
-		System.out.println("Tamanho do ArrayList: " + pessoas.size()); 
+		System.out.println("--- Array List --");
+		
+		System.out.println("Tamanho do ArrayList: " + pessoas.size());
 		
 		pessoas.add(pessoa1);
 		
-		System.out.println("Tamanho do ArrayList após novas inserções: " + pessoas.size()); 
+		System.out.println("Tamanho do ArrayList após novas inserções: " + pessoas.size());
 		System.out.println("O ArrayList possue a pessoa1 ? " + pessoas.contains(pessoa1));
 		System.out.println("Primeira pessoa do array é: "+ pessoas.get(0).getNome());
 		
@@ -35,7 +40,33 @@ public class CollectionsExample {
 		idades = pessoas.stream().mapToInt(p-> p.getIdade()).sum();
 
 		System.out.println("A soma das idades das pessoas, sem o elemento repetido é: "+ idades);
+
+
 		
+		/*Operações Com Set*/
+		pessoasSemRepeticao.add(pessoa1);
+		pessoasSemRepeticao.add(pessoa2);
+		pessoasSemRepeticao.add(pessoa3);
+		
+		System.out.println("--- Hash Set --");
+
+		
+		System.out.println("Tamanho do HashSet: " + pessoasSemRepeticao.size());
+		
+		pessoasSemRepeticao.add(pessoa1);
+				
+		/*Tamanho não é alterado*/
+		System.out.println("Tamanho do HashSet após 'elemento repetido': " + pessoasSemRepeticao.size());
+		System.out.println("O HashSet possue a pessoa1 ? " + pessoasSemRepeticao.contains(pessoa1));
+		System.out.println("Primeira pessoa do hashSet é: "+ pessoasSemRepeticao.stream().findFirst().get().getNome());
+		
+		idades = pessoasSemRepeticao.stream().mapToInt(p-> p.getIdade()).sum();
+		
+		System.out.println("A soma das idades das pessoas presentes no HashSet é: "+ idades);
+
+
+		
+				
 
 
 	}
